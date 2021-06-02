72cd1eb03ea4a0f3e45eb28fd311e04ce730b316
Francisco <francisco@codacy.com>
2021-06-02 14:14:35 +0100

    fix: Skip suggestion if all fields are undefined

--- src/computeSuggestion.ts
+++ src/computeSuggestion.ts
@@ -3,10 +3,14 @@ import { FixInfo } from "markdownlint"
 export function computeSuggestion(
     lineContent: string,
     fixInfo: FixInfo
-): string {
-    const column = fixInfo.editColumn ? fixInfo.editColumn - 1 : 0
-    const newText = fixInfo.insertText ? fixInfo.insertText : ""
-    const deleteCount = fixInfo.deleteCount ? fixInfo.deleteCount : 0
-    const result = lineContent.slice(0, column) + newText + lineContent.slice(column + deleteCount)
-    return result
+): string | undefined {
+    if (fixInfo.editColumn || fixInfo.insertText || fixInfo.deleteCount) {
+        const column = fixInfo.editColumn ? fixInfo.editColumn - 1 : 0
+        const newText = fixInfo.insertText ? fixInfo.insertText : ""
+        const deleteCount = fixInfo.deleteCount ? fixInfo.deleteCount : 0
+        const result = lineContent.slice(0, column) + newText + lineContent.slice(column + deleteCount)
+        return result
+    } else {
+        return undefined
+    }
 }
