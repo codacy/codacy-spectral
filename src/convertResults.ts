a43cbd3092dce3b39a1e66c0ae224eec54a56cff
Pedro Pereira <pedro.pereira@Pedros-MBP.lan>
2021-10-20 16:41:34 +0100

    file fetching working, ruleset fetching working, intial issue mapping WIP

--- src/convertResults.ts
+++ src/convertResults.ts
@@ -2,16 +2,14 @@ import { Issue } from "codacy-seed"
 import { ISpectralDiagnostic } from "@stoplight/spectral-core"
 
 export function convertResults(
-  report: ISpectralDiagnostic[],
-  filenameToContent: { [x: string]: string }
+  report: ISpectralDiagnostic[]
 ): Issue[] {
-  return Object.entries(report).flatMap((entry) => {
-    let [filename, issue] = entry
-    const fileContent = filenameToContent[filename]
+  return report.map((issue) => {
+    const filename = issue.source
     const lineNumber = issue.range.start.line
     const message = issue.message
-    const patternId = ""
-    const suggestion = issue.message
-    return new Issue(filename, message, patternId, lineNumber, suggestion)
+    const patternId = issue.code as string 
+    const suggestion = ""
+    return new Issue(filename!, message, patternId, lineNumber, suggestion)
   })
 }
