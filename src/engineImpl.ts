1ada49472c14677483ae9d3e9b68931dfdbba40b
Pedro Pereira <pedro.pereira@Pedros-MBP.lan>
2021-10-20 16:41:34 +0100

    file fetching working, ruleset fetching working, intial issue mapping WIP

--- src/engineImpl.ts
+++ src/engineImpl.ts
@@ -2,78 +2,38 @@ import { Codacyrc, Engine, ToolResult } from "codacy-seed"
 
 import { convertResults } from "./convertResults"
 import { Spectral, Document, Ruleset, RulesetDefinition } from "@stoplight/spectral-core"
-import { falsy } from '@stoplight/spectral-functions';
 import { readFile } from "codacy-seed"
-import {Yaml} from "@stoplight/spectral-parsers"
+import { Json, Yaml } from "@stoplight/spectral-parsers"
+import { oas, asyncapi } from "@stoplight/spectral-rulesets"
+import * as glob from "glob"
 
 export const engineImpl: Engine = async function (
   codacyrc?: Codacyrc
 ): Promise<ToolResult[]> {
 
-  const ruleset: RulesetDefinition = {
-    rules: {
-      'valid-type': {
-        given: '$..type',
-        then: {
-          function: falsy,
-        },
-      },
-    },
-    overrides: [
-      {
-        files: ['**/*.json#/foo/type'],
-        rules: {
-          'valid-type': 'info',
-        },
-      },
-      {
-        files: ['**/*.json', '**/*.json#/type'],
-        rules: {
-          'valid-type': 'error',
-        },
-      },
-      {
-        files: ['**/*.json#/bar/type'],
-        rules: {
-          'valid-type': 'off',
-        },
-      },
-      {
-        files: ['**/*.json#/bar/type/foo/type'],
-        rules: {
-          'valid-type': 'hint',
-        },
-      },
-    ],
-  };
-
   const spectral = new Spectral();
 
-  spectral.setRuleset(new Ruleset(ruleset, { source: "./docs/multiple-tests/without-config-file/src/.spectral.json"}));
+  spectral.setRuleset(new Ruleset({
+    extends: [oas, asyncapi]
+  }));
 
-  console.log("Spectral ruleset" + spectral.ruleset?.rules)
+  const codacyrcFiles = codacyrc && codacyrc.files ? codacyrc.files : glob.sync("**/*.json")
 
   const files = await Promise.all(
-    codacyrc?.files?.map(async (file) => {
-      const fileContent = await readFile(file)
+    codacyrcFiles.map(async (file) => {
+      const fileContent = readFile(file)
       return [file, fileContent.toString()]
     }) || []
   )
 
   const spectralResults = await Promise.all(
-    files.map(file => file[1]).map((fileContent) => {
-      const myDocument = new Document(fileContent, Yaml)
+    files.map((file) => {
+      const myDocument = new Document(file[1], Json, file[0])
       return spectral.run(myDocument)
     }).flat()
-    
   )
 
-  const spectralResultsModified = spectralResults.flat()
-
-  console.log("Spectral results" + spectralResultsModified)
-
   return convertResults(
-    spectralResultsModified,
-    Object.fromEntries(files)
+    spectralResults.flat()
   )
 }
