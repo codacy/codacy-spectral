f98488368dd7e39ed2fa28dd8c7b687a28852d1f
Luis Ventura <luis.ventura@codacy.com>
2021-10-21 15:03:39 +0100

    Fix pattern to detect json and yaml files
    Previous commit had the wrong pattern

--- src/engineImpl.ts
+++ src/engineImpl.ts
@@ -1,10 +1,10 @@
 import { Codacyrc, Engine, ToolResult } from "codacy-seed"
 
 import { convertResults } from "./convertResults"
-import { Spectral, Document, Ruleset } from "@stoplight/spectral-core"
+import { Spectral, Document } from "@stoplight/spectral-core"
 import { readFile } from "codacy-seed"
 import { Yaml, Json } from "@stoplight/spectral-parsers"
-import { oas, asyncapi } from "@stoplight/spectral-rulesets"
+const { oas, asyncapi } = require("@stoplight/spectral-rulesets");
 import * as glob from "glob"
 
 export const engineImpl: Engine = async function (
@@ -13,15 +13,15 @@ export const engineImpl: Engine = async function (
 
   const spectral = new Spectral();
 
-  spectral.setRuleset(new Ruleset({
-    extends: [oas, asyncapi]
-  }));
+  spectral.setRuleset({
+        extends: [oas, asyncapi]
+    });
 
-  const codacyrcFiles = codacyrc && codacyrc.files ? codacyrc.files : glob.sync("**/*.json")
+  const codacyrcFiles = codacyrc && codacyrc.files ? codacyrc.files : glob.sync("**/*.+(json|yaml|yml)")
 
   const files = await Promise.all(
     codacyrcFiles.map(async (file) => {
-      const fileContent = readFile(file)
+      const fileContent = await readFile(file)
       return [file, fileContent.toString()]
     }) || []
   )
@@ -30,6 +30,7 @@ export const engineImpl: Engine = async function (
     files.map((file) => {
       const filename = file[0];
       const extension = file[0].substring(filename.lastIndexOf('.') + 1, filename.length) || filename;
+      console.log(extension)
       const myDocument = extension === "json" ? new Document(file[1], Json, filename) : new Document(file[1], Yaml, filename)
       return spectral.run(myDocument)
     }).flat()
