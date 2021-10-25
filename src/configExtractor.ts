b695bf509969ce51b8412936c3ad9ddae8c3331f
Pedro Pereira <>
2021-10-25 12:31:21 +0100

    refactor of integration tests, added config extractor tests, change file name to config extractor

--- src/configExtractor.ts
+++ src/configExtractor.ts
@@ -1,13 +1,11 @@
-import { Codacyrc, Pattern } from "codacy-seed"
+import { Codacyrc } from "codacy-seed"
 import { toolName } from "./toolMetadata"
 import * as glob from "glob"
 
 export async function extractPatternIdsToApply(
     codacyrc?: Codacyrc
 ): Promise<string[] | undefined> {
-
     const tool = codacyrc?.tools ? codacyrc?.tools.find((codacyTool) => codacyTool.name === toolName) : undefined
-
     return tool?.patterns?.map(pattern => pattern.patternId)
 }
 
